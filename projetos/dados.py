e709b6f95770c28b0eedefb0e8cc1abd036f59c4
Victor Machado da Silva <vmachado@muxi.com.br>
2021-02-20 13:18:30 -0300

    Incluídos projetos para AP1

---
+++ projetos/dados.py
@@ -0,0 +1,35 @@
+"""
+Projeto 1 da disciplina de Data Mining com Python.
+"""
+import random
+
+
+def rola_dados(dado, qtde):
+    """
+    Rola um determinado número de dados e os imprime lado a lado.
+
+    Argumentos:
+    dado (int) - O tamanho do dado.
+    qtde (int) - O número de dados a serem rolados.
+
+    Retorna: None
+    """
+
+
+def valida_dados(info):
+    """
+    Retorna True se um determinado dado é numérico maior que zero.
+    Imprime na tela caso seja um dado inválido.
+
+    Argumento:
+    info (str) - A informação que precisa ser validada.
+
+    Retorna: bool - True se o dado for numérico, False caso contrário
+    """
+
+
+def main():
+    """Função principal, contém a lógica principal do programa."""
+
+
+main()
